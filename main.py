371cf7e93fec49d578c81d7a78f227cfa74aa4b9
dadnator <massonhalima29@gmail.com>
2025-08-24 21:04:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -208,7 +208,7 @@ class MastermindGameView(discord.ui.View):
                 color=discord.Color.green()
             )
             result_embed.add_field(name="Tentatives", value=f"{len(self.historique_tours)}/{MAX_TENTATIVES}", inline=False)
-            result_embed.add_field(name="🏆 Gagnant", value=f"**{joueur2.mention}** remporte **{net_gain:,}".replace(",", "\u00A0") + "\u00A0kamas** 💰 (après 5% de commission)", inline=False)
+            result_embed.add_field(name="🏆 Gagnant", value=f"**{joueur2.mention}** remporte **{format(montant_gagne, ',').replace(',', ' ')}** kamas 💰 (après 5% de commission)", inline=False)
         else:
             gagnant_id = joueur1.id
             result_embed = discord.Embed(
@@ -217,7 +217,7 @@ class MastermindGameView(discord.ui.View):
                 color=discord.Color.red()
             )
             result_embed.add_field(name="Tentatives", value=f"{len(self.historique_tours)}/{MAX_TENTATIVES}", inline=False)
-            result_embed.add_field(name="🏆 Gagnant", value=f"**{joueur1.mention}** remporte **{net_gain:,}".replace(",", "\u00A0") + "\u00A0kamas** 💰 (après 5% de commission)", inline=False)
+            result_embed.add_field(name="🏆 Gagnant", value=f"**{joueur1.mention}** remporte **{format(montant_gagne, ',').replace(',', ' ')}** kamas 💰 (après 5% de commission)", inline=False)
         
         await self.game_message.edit(embed=result_embed, view=None)
         
